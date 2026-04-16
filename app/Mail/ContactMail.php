6492d17fa5c69dcca76d7ec1221953edf051436b
İbrahim Parlak <ibrahimparlak819@gmail.com>
2026-04-16 22:14:46 +0300

    Update ContactMail.php

--- app/Mail/ContactMail.php
+++ app/Mail/ContactMail.php
@@ -1,52 +1,45 @@
 <?php
-
 namespace App\Mail;
-
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Mail\Mailables\Address;
-
 class ContactMail extends Mailable
 {
     use Queueable, SerializesModels;
-
     public $name;
     public $email;
     public $userMessage;
     public $subject;
-
     public function __construct($data)
     {
-        $this->name = $data['name'];
-        $this->email = $data['email']; // Kullanıcının emaili
+        $this->name        = $data['name'];
+        $this->email       = $data['email'];
         $this->userMessage = $data['message'];
-        $this->subject = $data['subject'] ?? 'New Communication Message';
+        $this->subject     = $data['subject'] ?? 'New Contact Message';
     }
-
     public function envelope(): Envelope
     {
         return new Envelope(
-            from: new Address('ibrahimparlak282@gmail.com', 'Miharbi Clothing'), // Sizden gider
-            replyTo: [new Address($this->email, $this->name)], // Cevap kullanıcıya gider
+            from: new Address('ibrahimparlak282@gmail.com', 'Miharbi Clothing'),
+            replyTo: [new Address($this->email, $this->name)],
             subject: $this->subject,
         );
     }
-
     public function content(): Content
     {
         return new Content(
             view: 'emails.contact',
             with: [
-                'name' => $this->name,
-                'email' => $this->email,
+                'name'        => $this->name,
+                'email'       => $this->email,
                 'userMessage' => $this->userMessage,
+                'subject'     => $this->subject,
             ],
         );
     }
-
     public function attachments(): array
     {
         return [];
